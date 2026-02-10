00845c9eb12c90958af9c6cadcbf5fad4068a8f2
anjose30 <andresjmo2013@email.com>
2026-02-10 15:05:01 -0500

    feat: add initial structure for attach, audit, audit_config, base, change_model, company, function, and travel_routes apps

---
+++ apps/audit_config/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AuditConfigConfig(AppConfig):
+    name = "audit_config"
---
+++ apps/change_model/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChangeModelConfig(AppConfig):
+    name = "change_model"
